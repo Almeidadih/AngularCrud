67d01ca26f153e82b9114cee4d8ccaf581327ada
diego <almeidadieo488@gmail.com>
2025-11-24 16:49:48 -0300

    Começando a construir front estilização

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-    exibirLogo: boolean = false;
 
-    itensMenu =[
-      { label: 'Home', link: '' },
-      { label: 'Cadastro', link: '/pessoas/incluir' },
-      { label: 'Login', link: '/pessoas/login' },
-      { label: 'Sobre', link: '/sobre' },
-      { label: 'Ajuda', link: '/ajuda' },
-    ];
 
 }
